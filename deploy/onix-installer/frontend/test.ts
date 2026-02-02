448a30538dac97152a7e8b202ccba5c20edb5967
pojoshi <pojoshi@google.com>
2026-02-02 11:51:17 +0000

    Make Angular frontend compatible for google3
    This CL updates the Angular code within the //third_party/dpi_becknonix/deploy/onix_installer/frontend directory to build and run within the google3 environment.
    
    Key changes include:
    - Added/modified BUILD files using ng_module rules.
    - Adjusted TypeScript imports to google3 paths.
    - Conformed code to google3 style guides. The code changes are documented here https://docs.google.com/document/d/1YN2H1fXBuUUycOcCr8Hco2xmJA5s3qZQNbbjekU15xU/edit?tab=t.0
    
    **EXPECTED TAP FAILURES:** TAP Presubmit is expected to fail for this CL because this CL only fixes the angular code, TAP failure indicates the failure due to Go and python test cases failing in this project but these fixes will be done as part of the separate CLs.
    
    Tested:
    1. blaze build //third_party/dpi_becknonix/deploy/onix_installer/frontend/...  (Succeeded)
    2. blaze test //third_party/dpi_becknonix/deploy/onix_installer/frontend/... (All tests passed)
    PiperOrigin-RevId: 864282331

--- deploy/onix-installer/frontend/test.ts
+++ deploy/onix-installer/frontend/test.ts
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-import 'zone.js';
-import 'zone.js/testing';
+// This file is required by karma.conf.js and loads recursively all the .spec
+// and framework files
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
@@ -28,3 +27,5 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+
+export {};
