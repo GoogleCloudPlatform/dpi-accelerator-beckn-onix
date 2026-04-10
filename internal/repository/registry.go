6980645aa406ba0614de30ff3d280f63eb1bdb6a
arpitbhardwaj <arpitbhardwaj@google.com>
2026-04-10 12:42:34 +0000

    Updated license from 'Copyright 2025 Google LLC' to  'Copyright 2026 Google LLC' Added copybara transformation to append license on top of the file if it is not present
    PiperOrigin-RevId: 897644377

--- internal/repository/registry.go
+++ internal/repository/registry.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import (
 
 	"cloud.google.com/go/cloudsqlconn"
 	"cloud.google.com/go/cloudsqlconn/postgres/pgxv5"
+
 	// Import postgres dialect for goqu.
 	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
 	"github.com/doug-martin/goqu/v9"
